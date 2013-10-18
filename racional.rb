72088909f9a4862cd99c9f139709fd9604c997f5
NUNEZ RODRIGUEZ <alu3573@cc171.(none)>
2013-10-18 12:27:15 +0100

    clase racional version 1

--- racional.rb
+++ racional.rb
@@ -3,4 +3,32 @@
 require "./gcd.rb"
 
 class Fraccion
+   def initialize(a, b)
+      #atributos
+      @numerador = a
+      @denominador = b
+   end
+   
+   def to_s()
+      puts "#{@numerador} / #{@denominador}"
+   end
+   
+   def suma (fraccion1)
+     
+   end
+   
+   def resta (fraccion1)
+     
+   end
+   
+   def producto (fraccion1)
+   
+   end
+   
+   def division (fraccion1)
+    
+   end
 end
+
+fraccion2 = Fraccion.new(2,1)
+fraccion2.to_s();
